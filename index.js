52bcd7539e3ca9198ed989584e120adb32ba307b
Tobias Liebig <work@etobi.de>
2017-05-10 10:04:13 +0200

    refactor

--- index.js
+++ index.js
@@ -12,36 +12,83 @@ var sendToLcn = function(command) {
   	});
 };
 
+var dashConfig = {
+	buttons: {
+		"50:f5:da:15:19:c9": {
+			name: "playdoh",
+			lcn: "LCN.sendkey.0.12.B8=hit"
+		},
+		"50:f5:da:9d:31:f4": {
+			name: "power point",
+			lcn: "LCN.sendkey.0.60.A2=hit"
+		},
+		"88:71:e5:5d:9e:7e": {
+			name: "nerf",
+			lcn: "LCN.sendkey.0.70.A2=hit"
+		},
+		"50:f5:da:80:ce:d8": {
+			name: "barbantia",
+			lcn: "LCN.sendkey.0.50.A1=hit"
+		},
+		"ac:63:be:1a:1e:f7": {
+			name: "somat/ohne",
+			lcn: ""
+		},
+		"ac:63:be:6b:4f:41": {
+			name: "nobo",
+			lcn: ""
+		},
+		"ac:63:be:e0:5c:12": {
+			name: "dreamies",
+			lcn: ""
+		},
+		"50:f5:da:d3:b0:ac": {
+			name: "eukanuba",
+			lcn: ""
+		},
+		"f0:27:2d:c1:55:1d": {
+			name: "bio zentrale",
+			lcn: ""
+		},
+		"b4:7c:9c:9f:2c:3f": {
+			name: "durex",
+			lcn: ""
+		}
+	}
+};
+
+var sendSlackMessage = function (message) {
+	var options = {
+		uri: 'https://hooks.slack.com/services/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',
+		method: 'POST',
+		json: {
+			"text": message
+		}
+	};
+	request(options, function (error, response, body) {
+		if (error) {
+			console.log('Error! (sendMessage/request)', error);
+		}
+	});
+};
+
 var dash_button = require('node-dash-button');
-var dash = dash_button(["50:f5:da:15:19:c9", "50:f5:da:9d:31:f4", "ac:63:be:1a:1e:f7", "88:71:e5:5d:9e:7e"], null, null, "all");
+
+sendSlackMessage(Object.keys(dashConfig).join(', '));
+var dash = dash_button(Object.keys(dashConfig), null, null, "all");
+
+
 dash.on("detected", function (id){
-
-  if (id == "50:f5:da:15:19:c9") {
-  	console.log("playdoh");
-	sendToLcn('LCN.sendkey.0.12.B8=hit');
-  }
-
-  if (id == "50:f5:da:9d:31:f4") {
-  	console.log("power point");
-	sendToLcn('LCN.sendkey.0.60.A2=hit');
-  }
-
-  if (id == "88:71:e5:5d:9e:7e") {
-  	console.log("nerf");
-	sendToLcn('LCN.sendkey.0.70.A2=hit');
-  }
-
-  if (id == "50:f5:da:80:ce:d8") {
-  	console.log("barbantia");
-	sendToLcn('LCN.sendkey.0.50.A1=hit');
-  }  
-
-// ac:63:be:1a:1e:f7	somat / ohne label
-// ac:63:be:6b:4f:41	nobo
-// ac:63:be:03:2e:9a	rexel
-// 50:f5:da:80:ce:d8 	barbantia
-// ac:63:be:e0:5c:12	dreamies
-// 50:f5:da:d3:b0:ac 	eukanuba
-// f0:27:2d:c1:55:1d	bio zentrale
-// b4:7c:9c:9f:2c:3f	durex ?
+	Object.keys(dashConfig).forEach(function(address) {
+		if (address == id) {
+			var button = dashConfig[address];
+			if (button && button.name) {
+				console.log(button.name);
+				sendSlackMessage(button.name);
+			}
+			if (button && button.lcn &&  button.lcn !== "") {
+				sendToLcn(button.lcn);
+			}
+		}
+	});
 });
