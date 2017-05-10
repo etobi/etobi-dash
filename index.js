c82acf6efd53f1d935a76bebfecbc714f2a70bbb
Tobias Liebig <work@etobi.de>
2017-05-10 09:36:04 +0200

    neuer button für garage

--- index.js
+++ index.js
@@ -31,8 +31,17 @@ dash.on("detected", function (id){
 	sendToLcn('LCN.sendkey.0.70.A2=hit');
   }
 
-  if (id == "ac:63:be:1a:1e:f7") {
-  	console.log("white");
+  if (id == "50:f5:da:80:ce:d8") {
+  	console.log("barbantia");
 	sendToLcn('LCN.sendkey.0.50.A1=hit');
-  }
+  }  
+
+// ac:63:be:1a:1e:f7	somat / ohne label
+// ac:63:be:6b:4f:41	nobo
+// ac:63:be:03:2e:9a	rexel
+// 50:f5:da:80:ce:d8 	barbantia
+// ac:63:be:e0:5c:12	dreamies
+// 50:f5:da:d3:b0:ac 	eukanuba
+// f0:27:2d:c1:55:1d	bio zentrale
+// b4:7c:9c:9f:2c:3f	durex ?
 });
