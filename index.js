68672086f8ea34f5a894a58a19663a7ba35606af
Tobias Liebig <work@etobi.de>
2017-05-24 22:16:29 +0200

    configure button

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var dashConfig = {
 		},
 		"50:f5:da:9d:31:f4": {
 			name: "power point",
-			http: "http://192.168.50.105/cm?cmnd=PowerOnState%20toggle"
+			http: "http://192.168.50.103/cm?cmnd=Power1%20toggle"
 		},
 		"88:71:e5:5d:9e:7e": {
 			name: "nerf",
