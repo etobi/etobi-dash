7be08d6beeef7bb8be6e77aa550d069b1840e52a
Tobias Liebig <work@etobi.de>
2017-06-02 14:11:27 +0200

    configure dashbuttons

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ var dashConfig = {
 	buttons: {
 		"50:f5:da:15:19:c9": {
 			name: "playdoh",
-			lcn: "LCN.sendkey.0.12.B8=hit"
+			lcn: "LCN.sendkey.0.12.B8=hit" // Steckdosen aussen
 		},
 		"50:f5:da:9d:31:f4": {
 			name: "power point",
-			http: "http://192.168.50.103/cm?cmnd=Power1%20toggle"
+			http: "http://192.168.50.103/cm?cmnd=Power1%20toggle" // Silvia
 		},
 		"88:71:e5:5d:9e:7e": {
 			name: "nerf",
-			lcn: "LCN.sendkey.0.70.A2=hit"
+			lcn: "LCN.sendkey.0.70.A2=hit" // Buero Steckdose
 		},
 		"50:f5:da:80:ce:d8": {
 			name: "barbantia",
-			lcn: "LCN.sendkey.0.50.A1=hit"
+			lcn: "LCN.sendkey.0.50.A1=hit" // Garage
 		},
 		"ac:63:be:1a:1e:f7": {
 			name: "somat/ohne",
@@ -37,7 +37,7 @@ var dashConfig = {
 		},
 		"ac:63:be:6b:4f:41": {
 			name: "nobo",
-			lcn: ""
+			lcn: "LCN.sendkey.0.50.A1=hit" // Garage
 		},
 		"ac:63:be:e0:5c:12": {
 			name: "dreamies",
@@ -54,6 +54,10 @@ var dashConfig = {
 		"b4:7c:9c:9f:2c:3f": {
 			name: "durex",
 			lcn: ""
+		},
+		"ac:63:be:03:2e:9a": {
+			name: "rexel",
+			lcn: "LCN.sendkey.0.63.A1=hit" // Licht Terrasse
 		}
 	}
 };
